ac088bfd417938989632f92ef78c9d3abd044540
android <alessandro.falcioni@matteirecanati.org>
2023-03-07 12:58:46 +0100

    calcolatriceee

---
+++ app/src/main/java/com/example/calcolatrice/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.calcolatrice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button somma =(Button) findViewById(R.id.btnsomma);
+        EditText v1 =(EditText) findViewById(R.id.editTextN1);
+        EditText v2=(EditText) findViewById(R.id.editTextN2);
+        TextView tot=(TextView) findViewById(R.id.textViewRisultato);
+
+        somma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int totale=0;
+                totale=Integer.valueOf(v1.getText().toString()) + Integer.valueOf(v2.getText().toString());
+                tot.setText(""+totale);
+
+            }
+        });
+    }
+}
